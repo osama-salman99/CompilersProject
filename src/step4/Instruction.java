7e4049437b3301eadb22113c5b939ae12d12fad8
osama <osama.salman99@gmail.com>
2021-05-03 11:03:18 +0300

    update

--- src/step4/Instruction.java
+++ src/step4/Instruction.java
@@ -1,6 +1,6 @@
 package step4;
 
-public class Operation {
+public class Instruction {
 	private final String opCode;
 	private final Symbol operand1;
 	private final Symbol operand2;
@@ -10,32 +10,32 @@ public class Operation {
 	/**
 	 * @param opCode ADDI / ADDF / SUBI / SUBF / MULTI / MULTF / DIVI / DIVF
 	 */
-	public Operation(String opCode, Symbol operand1, Symbol operand2, Symbol result) {
+	public Instruction(String opCode, Symbol operand1, Symbol operand2, Symbol result) {
 		this(opCode, operand1, operand2, result, null);
 	}
 
 	/**
 	 * @param opCode GT / GE / LT / LE / NE / EQ
 	 */
-	public Operation(String opCode, Symbol operand1, Symbol operand2, String label) {
+	public Instruction(String opCode, Symbol operand1, Symbol operand2, String label) {
 		this(opCode, operand1, operand2, null, label);
 	}
 
 	/**
 	 * @param opCode JUMP / LABEL
 	 */
-	public Operation(String opCode, String label) {
+	public Instruction(String opCode, String label) {
 		this(opCode, null, null, null, label);
 	}
 
 	/**
 	 * @param opCode READI / READF / WRITEI / WRITEF / WRITES
 	 */
-	public Operation(String opCode, Symbol result) {
+	public Instruction(String opCode, Symbol result) {
 		this(opCode, null, null, result, null);
 	}
 
-	private Operation(String opCode, Symbol operand1, Symbol operand2, Symbol result, String label) {
+	private Instruction(String opCode, Symbol operand1, Symbol operand2, Symbol result, String label) {
 		this.opCode = opCode;
 		this.operand1 = operand1;
 		this.operand2 = operand2;
